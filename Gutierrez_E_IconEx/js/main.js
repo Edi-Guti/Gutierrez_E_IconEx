a655d20df638da78844916ef443e2467e2fb8ee1
Edi-Guti <50373581+Edi-Guti@users.noreply.github.com>
2019-05-21 17:44:00 -0400

    Add files via upload

--- Gutierrez_E_IconEx/js/main.js
+++ Gutierrez_E_IconEx/js/main.js
@@ -8,18 +8,20 @@
 	// and using a CSS slector to find the element we want to use.
 
 	//;let stores the reference to element as a variable (in memory)
-	let the svgGraphic = document.querySelector("#badgeSVG");
-			mainHeadline = document.querySelector("main-headline");
-			subHead = document.querySelector("#raster-vector h3"),
-			swapTextButton = document.querySelector(".switch-type");
+	let mainHeadline = document.querySelector("main-headline"),
+		subHead = document.querySelector("#raster-vector h3"),
+		swapTextButton = document.querySelector(".switch-type"),
+		allImages = document.querySelector("#image-container img"),
+		objectButton + document.querySelector(".container button")
 
 
 	// functions are reusable pieces of code - put these between the variables and the event handling
 	// that you can run any time
 	function logMyId() {
 		console.log(this.id);
+		// toggle a class here with classlist
 
-		this.style.opacity = 0.5;
+		// this.style.opacity = 0.5;
 	}
 
 	function swapText() {
@@ -31,8 +33,21 @@
 		mainHealine.classlist.toggle("selected");
 	}
 
+	function logTheSvg() {
+		console.log(this.previousElementSibling.id);
+	}
+
 
 	// events always go down here
-	svgGraphic.addEventListener("click", logMyId);
+	// svgGraphic.addEvenListener("click", logMyId);
 	swapTextButton.addEventListener("click", swapText);
+
+	// select and loop through a bunch of items at once -> one to many relationship
+	allImages.forEach(item => {
+		item.addEventListener("click", logMyId)
+	});
+
+	// the button beside the svg objectButton
+	objectButton.addEventListener("click", logMyId),
+
 })();
