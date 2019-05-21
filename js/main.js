b7f64f905466c4ef0d6211a6ef99d97c678b555d
Gutierrez <e_gutierrez@tss-hr420-dm21.fanshawec.ca>
2019-05-21 16:10:02 -0400

    add files

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@
 	console.log('fired!');
 
 	// select elements with JavaScript
-	
+
 	// setting up a variable using the let keyword,
 	// and using a CSS slector to find the element we want to use.
-	
-	//;et stores the reference to element as a variable (in memory)
+
+	//;let stores the reference to element as a variable (in memory)
 	let the svgGraphic = document.querySelector("#badgeSVG");
+			mainHeadline = document.querySelector("main-headline");
 
 	// functions are reusable pieces of code
 	// that you can run any time
@@ -20,4 +21,5 @@
 
 	svgGraphic.addEventListener("click", logMyId);
 
+	mainHeadline.textContent = "Now you're something else!";
 })();
