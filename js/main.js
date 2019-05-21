b65bc6c5881d5ef5aadbc66ddb9cdbed130e1c21
Gutierrez <e_gutierrez@tss-hr420-dm21.fanshawec.ca>
2019-05-21 16:46:40 -0400

    adding changes

--- js/main.js
+++ js/main.js
@@ -10,8 +10,11 @@
 	//;let stores the reference to element as a variable (in memory)
 	let the svgGraphic = document.querySelector("#badgeSVG");
 			mainHeadline = document.querySelector("main-headline");
+			subHead = document.querySelector("#raster-vector h3"),
+			swapTextButton = document.querySelector(".switch-type");
 
-	// functions are reusable pieces of code
+
+	// functions are reusable pieces of code - put these between the variables and the event handling
 	// that you can run any time
 	function logMyId() {
 		console.log(this.id);
@@ -19,7 +22,17 @@
 		this.style.opacity = 0.5;
 	}
 
-	svgGraphic.addEventListener("click", logMyId);
+	function swapText() {
+		mainHeadline.textContent = "Now you're something else!";
+		mainHealine.classlist.toggle("selected");
 
-	mainHeadline.textContent = "Now you're something else!";
+
+		subHead.textContent = "You are also something else!";
+		mainHealine.classlist.toggle("selected");
+	}
+
+
+	// events always go down here
+	svgGraphic.addEventListener("click", logMyId);
+	swapTextButton.addEventListener("click", swapText);
 })();
